7d9c5deb612257260ff53efcca94541520ae4b63
Clement VIDON <cvidon@student.42.fr>
2025-04-19 16:35:05 +0200

    feat: interpret output using LLMs

--- src/agent/runAgent.ts
+++ src/agent/runAgent.ts
@@ -2,19 +2,13 @@ import { getRedditDataPoints } from '../reddit/redditData.ts';
 import { filterDataPoints } from './filterDataPoints';
 import { analyzeSentiments } from './analyzeSentiments.ts';
 import { compressSentiments } from './compressSentiments.ts';
+import { interpretSentiment } from './interpretSentiment.ts';
 
 export const runAgent = async (): Promise<string[]> => {
   const dataPoints = await getRedditDataPoints('developpeurs', 100, 'week');
-  console.log(dataPoints);
-
   const relevantDataPoints = await filterDataPoints(dataPoints);
-  console.log(relevantDataPoints);
-
   const sentiments = await analyzeSentiments(relevantDataPoints);
-  console.log(sentiments);
-
   const sentiment = await compressSentiments(sentiments);
-  console.log(sentiment);
-
-  return 'done';
+  const report = await interpretSentiment(sentiment);
+  return report;
 };
