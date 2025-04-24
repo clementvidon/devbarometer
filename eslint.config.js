dc82a5074df9404558e2939f79cd538d074c15dc
Clement VIDON <cvidon@student.42.fr>
2025-04-24 10:37:22 +0200

    chore: update ESLint config
    - flat config with TypeScript and Vitest support

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,58 @@
 import { defineConfig } from 'eslint/config';
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import vitest from 'eslint-plugin-vitest';
 
 export default defineConfig([
   { ignores: ['node_modules', 'public'] },
+
+  // JS config
   {
-    files: ['**/*.{js,mjs,cjs,ts}'],
+    files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
     extends: ['js/recommended'],
   },
+
+  // TS config
   {
-    files: ['**/*.{js,mjs,cjs,ts}'],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/strict-boolean-expressions': 'warn',
+      '@typescript-eslint/no-misused-promises': 'error',
+    },
   },
-  tseslint.configs.recommended,
+
+  // Vitest config
+  {
+    files: ['**/*.test.{ts,tsx}'],
+    plugins: { vitest },
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      globals: {
+        vi: true,
+        describe: true,
+        test: true,
+        expect: true,
+        beforeEach: true,
+        afterEach: true,
+        it: true,
+      },
+    },
+    rules: {},
+  },
 ]);
+
