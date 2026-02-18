0f053fd5831804e57263350b8b2cc1e8e908804b
cvidon <cvidon@student.42.fr>
2026-02-18 10:44:23 +0100

    refactor(tests): remove a useless describe level

--- backend/src/application/observability/withSpan.test.ts
+++ backend/src/application/observability/withSpan.test.ts
@@ -62,77 +62,73 @@ describe('withSpan', () => {
   afterEach(() => {
     vi.restoreAllMocks();
   });
-  describe('happy path', () => {
-    test('log the time of a task', async () => {
-      const { logger, childLogger } = makeLoggerDouble();
-      const name = 'my-span';
-      const fn = vi.fn(() => 'ok');
+  test('log the time of a task', async () => {
+    const { logger, childLogger } = makeLoggerDouble();
+    const name = 'my-span';
+    const fn = vi.fn(() => 'ok');
 
-      vi.spyOn(Date, 'now').mockReturnValueOnce(1000).mockReturnValueOnce(1300);
+    vi.spyOn(Date, 'now').mockReturnValueOnce(1000).mockReturnValueOnce(1300);
 
-      const result = await withSpan(logger, name, fn);
+    const result = await withSpan(logger, name, fn);
 
-      expect(result).toBe('ok');
-      expect(fn).toHaveBeenCalledTimes(1);
-      expect(logger.child).toHaveBeenCalledTimes(1);
-      expect(logger.child).toHaveBeenNthCalledWith(1, { span: name });
-      expect(childLogger.debug).toHaveBeenCalledTimes(2);
-      expect(childLogger.debug).toHaveBeenNthCalledWith(
-        1,
-        'start',
-        expect.objectContaining({ ts: 1000 }),
-      );
-      expect(childLogger.debug).toHaveBeenNthCalledWith(
-        2,
-        'end',
-        expect.objectContaining({ ts: 1300, durationMs: 300 }),
-      );
-    });
-    test('supports async task', async () => {
-      const { logger, childLogger } = makeLoggerDouble();
-      const name = 'my-span';
-      const fn = vi.fn(() => Promise.resolve('ok'));
-
-      vi.spyOn(Date, 'now').mockReturnValueOnce(1000).mockReturnValueOnce(1300);
-
-      const result = await withSpan(logger, name, fn);
-
-      expect(result).toBe('ok');
-      expect(fn).toHaveBeenCalledTimes(1);
-      expect(logger.child).toHaveBeenCalledTimes(1);
-      expect(logger.child).toHaveBeenNthCalledWith(1, { span: name });
-      expect(childLogger.debug).toHaveBeenCalledTimes(2);
-      expect(childLogger.debug).toHaveBeenNthCalledWith(1, 'start', {
-        ts: 1000,
-      });
-      expect(childLogger.debug).toHaveBeenNthCalledWith(
-        2,
-        'end',
-        expect.objectContaining({ ts: 1300, durationMs: 300 }),
-      );
-    });
+    expect(result).toBe('ok');
+    expect(fn).toHaveBeenCalledTimes(1);
+    expect(logger.child).toHaveBeenCalledTimes(1);
+    expect(logger.child).toHaveBeenNthCalledWith(1, { span: name });
+    expect(childLogger.debug).toHaveBeenCalledTimes(2);
+    expect(childLogger.debug).toHaveBeenNthCalledWith(
+      1,
+      'start',
+      expect.objectContaining({ ts: 1000 }),
+    );
+    expect(childLogger.debug).toHaveBeenNthCalledWith(
+      2,
+      'end',
+      expect.objectContaining({ ts: 1300, durationMs: 300 }),
+    );
   });
-  describe('error cases', () => {
-    test('throws if the given task throws', async () => {
-      const { logger, childLogger } = makeLoggerDouble();
-      const err = new Error('boom');
-      const fn = vi.fn(() => {
-        throw err;
-      });
+  test('supports async task', async () => {
+    const { logger, childLogger } = makeLoggerDouble();
+    const name = 'my-span';
+    const fn = vi.fn(() => Promise.resolve('ok'));
 
-      vi.spyOn(Date, 'now').mockReturnValueOnce(1000).mockReturnValueOnce(1300);
+    vi.spyOn(Date, 'now').mockReturnValueOnce(1000).mockReturnValueOnce(1300);
 
-      await expect(withSpan(logger, 'my-span', fn)).rejects.toBe(err);
+    const result = await withSpan(logger, name, fn);
 
-      expect(childLogger.debug).toHaveBeenNthCalledWith(
-        2,
-        'end',
-        expect.objectContaining({
-          ts: 1300,
-          durationMs: 300,
-          error: err,
-        }),
-      );
+    expect(result).toBe('ok');
+    expect(fn).toHaveBeenCalledTimes(1);
+    expect(logger.child).toHaveBeenCalledTimes(1);
+    expect(logger.child).toHaveBeenNthCalledWith(1, { span: name });
+    expect(childLogger.debug).toHaveBeenCalledTimes(2);
+    expect(childLogger.debug).toHaveBeenNthCalledWith(1, 'start', {
+      ts: 1000,
     });
+    expect(childLogger.debug).toHaveBeenNthCalledWith(
+      2,
+      'end',
+      expect.objectContaining({ ts: 1300, durationMs: 300 }),
+    );
+  });
+  test('throws if the given task throws', async () => {
+    const { logger, childLogger } = makeLoggerDouble();
+    const err = new Error('boom');
+    const fn = vi.fn(() => {
+      throw err;
+    });
+
+    vi.spyOn(Date, 'now').mockReturnValueOnce(1000).mockReturnValueOnce(1300);
+
+    await expect(withSpan(logger, 'my-span', fn)).rejects.toBe(err);
+
+    expect(childLogger.debug).toHaveBeenNthCalledWith(
+      2,
+      'end',
+      expect.objectContaining({
+        ts: 1300,
+        durationMs: 300,
+        error: err,
+      }),
+    );
   });
 });
